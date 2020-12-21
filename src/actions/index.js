2a42c15cf02b0c773e07e38ba1697fc1bba75e23
AdrewBraz <andbrazhnikov@gmail.com>
2020-12-21 03:07:05 +0300

    actions

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+// @ts-check
+import { fetchData } from '../reducers/storeSlice';
+import { modalStateOpen, modalStateClose, stateURL } from '../reducers/appSlice';
+
+export default {
+  fetchData,
+  modalStateOpen,
+  modalStateClose,
+  stateURL,
+};
